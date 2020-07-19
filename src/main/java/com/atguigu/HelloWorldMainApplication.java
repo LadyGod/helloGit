a02f988e4c164e8646f4ab1bb76b98f49d2fb255
tom_glb <gooMorning_glb@atguigu.com>
2020-07-19 17:06:52 +0800

    first commit

--- src/main/java/com/atguigu/HelloWorldMainApplication.java
+++ src/main/java/com/atguigu/HelloWorldMainApplication.java
@@ -11,5 +11,6 @@ public class HelloWorldMainApplication {
     public static void main(String[] args) {
         // Spring应用启动起来
         SpringApplication.run(HelloWorldMainApplication.class,args);
+        System.out.println("master分支");
     }
 }
